6ffac43d4aca7678f5cda1553316771bea81f244
alex-wjb <alexanderwjb@gmail.com>
2022-08-22 19:40:37 +0100

    rename html files

--- createNav.js
+++ createNav.js
@@ -5,9 +5,9 @@ template.innerHTML = `
   <ul class="nav">
     <!-- closing li omitted to remove inline-block whitespace between els-->
     <li class="navItem"> <a class="navLink" href="index.html">Home</a>
-    <li class="navItem"> <a class="navLink" href="Death.html">Deaths Data</a>
-    <li class="navItem"> <a class="navLink" href="Testlocation.html">Test Center</a>
-    <li class="navItem"> <a class="navLink" href="NHSInformation.html">NHS Information</a>
+    <li class="navItem"> <a class="navLink" href="death.html">Deaths Data</a>
+    <li class="navItem"> <a class="navLink" href="test-location.html">Test Center</a>
+    <li class="navItem"> <a class="navLink" href="nhs-info.html">NHS Information</a>
     
   </ul>
 </nav>
